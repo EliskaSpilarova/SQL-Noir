d4d5e974f1c115aacf54a8a1783348691c839101
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-04-26 18:12:15 +0200

    Update The Vanishing Diamond.sql

--- The Vanishing Diamond.sql
+++ The Vanishing Diamond.sql
@@ -6,3 +6,7 @@ WHERE rental_date = '19870520'
     FROM attire_registry
     WHERE note LIKE '%navy suit%' AND note LIKE '%white tie%'
   );
+
+SELECT guest_id, confession
+FROM confessions
+WHERE guest_id IN (112, 145, 167, 190, 105);
